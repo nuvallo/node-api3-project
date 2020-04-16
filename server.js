7dce5196c68990e408fde6ffcfe9dcec1de68895
nuvallo <nuvallo.inc@gmail.com>
2020-04-16 16:25:14 -0400

    MVP completed

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRouter = require("./users/userRouter");
-
+const cors = require("cors");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(logger);
 
 const welcomeMsg = "Let's write some middleware";
 server.get("/", (req, res) => {
@@ -18,7 +20,11 @@ server.use((req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  req.time = Date.now();
+  console.log(`${req.method} to ${req.originalUrl} made at ${req.time}`);
+  next();
+}
 
 const hostName = process.env.hostName || "21.0.0.1";
 const PORT = process.env.PORT || 4000;
