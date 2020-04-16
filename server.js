e0660522e2980a3f956c70fddc5f5f037f7eede1
nuvallo <nuvallo.inc@gmail.com>
2020-04-15 22:18:33 -0400

    GET requests complete

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use(express.json());
+
+const welcomeMsg = "Let's write some middleware";
+server.get("/", (req, res) => {
+  res.send(`<h2>${welcomeMsg}</h2>`);
+});
+
+server.use("/users", userRouter);
+
+server.use((req, res) => {
+  res.status(400).json({ msg: "Route was not found" });
 });
 
 //custom middleware
 
 function logger(req, res, next) {}
+
+const hostName = process.env.hostName || "21.0.0.1";
+const PORT = process.env.PORT || 2000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${hostName}:${PORT}`);
+});
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,54 @@
-const express = require('express');
+const express = require("express");
+const db = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  db.get()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error retrieving users" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db.getById(req.params.id)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.log("Error", err);
+      res.status(500).json({ errorMessage: "Error retrieving user" });
+    });
+});
+
+router.get("/:id/posts", (req, res) => {
+  db.getUserPosts(req.params.id)
+    .then((posts) => {
+      res.status(201).json(posts);
+    })
+    .catch((err) => {
+      console.log("Error", err);
+      res.status(500).json({ errorMessage: "Error retrieving user posts" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
