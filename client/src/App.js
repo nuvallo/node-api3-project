2657de08b2763d5a28833d56bdc05bc0692e93cd
nuvallo <nuvallo.inc@gmail.com>
2020-04-16 17:16:10 -0400

    Grabbing users from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
 import Navbar from "./componets/layout/Navbar";
 import Homepage from "./componets/pages/Homepage";
 import Aboutpage from "./componets/pages/Aboutpage";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/users/`)
+      .then((res) => {
+        console.log("Users: ", res.data);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Route path="/" component={Navbar} />
-      <Route exact path="/" component={Homepage} />
+      <Route exact path="/" render={() => <Homepage users={users} />} />
       <Route path="/about" component={Aboutpage} />
     </div>
   );
